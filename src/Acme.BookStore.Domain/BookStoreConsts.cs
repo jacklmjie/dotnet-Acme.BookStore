f1e4d5d27b7d5867e8981e3469f116183ccb5e44
limengjie <li.mengjie@yalla.live>
2020-09-09 18:17:40 +0800

    添加abp示例

---
+++ src/Acme.BookStore.Domain/BookStoreConsts.cs
@@ -0,0 +1,9 @@
+﻿namespace Acme.BookStore
+{
+    public static class BookStoreConsts
+    {
+        public const string DbTablePrefix = "t_";
+
+        public const string DbSchema = null;
+    }
+}
